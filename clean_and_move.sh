d6ef2134ee48ffa968e82072a4d32c0b5ab44913
Prabhu <prabu.t@gmail.com>
2023-04-12 15:06:09 +0200

    first version

---
+++ clean_and_move.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if a path was provided as input
+if [ -z "$1" ]; then
+  echo "Please provide a path as input."
+  exit 1
+fi
+
+dst_folder="$2"
+
+log_file="$3"
+
+# clean file name 
+bash "/home/pbu80/scripts/clean_torrents.sh" "$1" >> "$log_file" 2>&1
+
+# move the file to a folder
+bash "/home/pbu80/scripts/movie2folder.sh" "$1" >> "$log_file" 2>&1
+
+#python "/home/pbu80/scripts/rename_folders.py" "$1" >> "$log_file" 2>&1
+
+# clean mkv file
+bash "/home/pbu80/scripts/mkvclean.v3.sh" "$1" >> "$log_file" 2>&1
+
+#move the 
+mv "$1"/* "$dst_folder"
+
